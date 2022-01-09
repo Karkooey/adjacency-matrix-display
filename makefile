c38e6b0a57fee82b1df0fd274511aa802c4654d7
clancylight <clancylight@gmail.com>
2022-01-09 15:32:27 +1300

    branch for compatibility

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ executableExtension=".o"
 exec=$(appName)$(executableExtension)
 
 build:
-	gcc -std=c99 ./src/*.c -lSDL2 -o $(exec)
+	gcc -std=c99 ./src/*.c 'sdl2-config --cflags --libs'   -I /usr/include/SDL2  -o $(exec)
 
 run:
 	./$(exec)
--- src/main.c
+++ src/main.c
@@ -4,14 +4,51 @@
  *  Created on: 9 Jan 2021
  *      Author: ClancyLight
  */
-#include "../inc/main.h"
+//#include "../inc/main.h"
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-int main(int argc, char* argv[]){
-	ctrl_initAll();
+#define FALSE 0
+#define TRUE  1
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
 
-	while(ctrl_getWantQuit()){
+
+
+
+static Window_t windowRend = {NULL, NULL};
+
+int view_initializeWindow(void) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL.\n");
+		return FALSE;
+	}
+	windowRend.window = SDL_CreateWindow(
+		"The Game",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		0
+	);
+	if (!windowRend.window) {
+		fprintf(stderr, "Error creating SDL Window.\n");
+		return FALSE;
+	}
+	windowRend.renderer = SDL_CreateRenderer(windowRend.window, -1, SDL_RENDERER_ACCELERATED);// try this later: SDL_RENDERER_ACCELERATED
+	if (!windowRend.renderer) {
+		fprintf(stderr, "Error Creating SDL Renderer.\n");
+		return FALSE;
+	}
+	return TRUE;
+}
+
+
+
+int main(int argc, char* argv[]){
+	//ctrl_initAll();
+
+	/*while(ctrl_getWantQuit()){
 
 		ctrl_processInput();
 		model_update();
@@ -21,3 +58,9 @@ int main(int argc, char* argv[]){
 	view_destroyWindow();
 	return 0;
 }
+*/
+	//testSDLEnvironment();
+	SDL_Delay(3000);
+	return 0;
+	
+	}
